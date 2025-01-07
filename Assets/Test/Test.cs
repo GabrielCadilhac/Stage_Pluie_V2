558ff9d30f0c50a02441b3a99740604737cb5404
GabrielCadilhac <59889782+GabrielCadilhac@users.noreply.github.com>
2025-01-07 09:33:56 +0100

    ~OBB collision working

--- Assets/Test/Test.cs
+++ Assets/Test/Test.cs
@@ -7,19 +7,13 @@ public class Test : MonoBehaviour
 
     private OBB _bounds;
 
-    private Vector3 _min, _max;
-
-    private Matrix4x4 _rotMatrix;
-
-    float _pointSize = 0.5f;
     float _boxSize = 9f;
-
 
     private struct OBB
     {
         public Vector3 center;
         public Vector3 size;
-        public Vector3 rotation;
+        public Matrix4x4 rotation;
     }
 
     private void Start()
@@ -27,40 +21,39 @@ public class Test : MonoBehaviour
         _bounds          = new OBB();
         _bounds.center   = new Vector3(0f, 0f , 0f);
         _bounds.size     = new Vector3(9f, 9f, 9f);
-        _bounds.rotation = new Vector3(0f, 0f, 0f);
+        _bounds.rotation = Matrix4x4.identity;
     }
 
     private void Update()
     {
         UpdateRotation();
         transform.localScale = Vector3.one * _boxSize * 2f;
-        Quaternion quat = new Quaternion();
-        quat.eulerAngles = new Vector3(-_rotation.x, _rotation.y, -_rotation.z) * (180f / Mathf.PI);
-        transform.rotation = rightCoordToUnityCord(quat);
+        transform.rotation   = _bounds.rotation.rotation;
 
-        if (TestCollision(_min, _max, _rotMatrix * _particle.position))
+        if (TestCollision(_particle.position, _bounds))
             _material.color = Color.green;
         else
             _material.color = Color.red;
-        
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            Debug.Log($"min {_min}");
-            Debug.Log($"max {_max}");
-            Debug.Log($"particle {_rotMatrix * _particle.position}");
-        }
     }
 
-    private Quaternion rightCoordToUnityCord(Quaternion q)
+    private bool TestCollision(Vector3 p_pos, OBB p_obb)
     {
-        return new Quaternion(q.x, q.y, -q.z, -q.w);
-    }
+        Vector3 D = p_pos - p_obb.center;
+        Vector3 ux = _bounds.rotation.GetColumn(0);
+        Vector3 uy = _bounds.rotation.GetColumn(1);
+        Vector3 uz = _bounds.rotation.GetColumn(2);
 
-    private bool TestCollision(Vector3 p_min, Vector3 p_max, Vector3 p_pos)
-    {
-        return p_min.x < p_pos.x && p_pos.x < p_max.x &&
-               p_min.y < p_pos.y && p_pos.y < p_max.y &&
-               p_min.z < p_pos.z && p_pos.z < p_max.z;
+        // Normalize ux, uy and uz
+        ux.Normalize();
+        uy.Normalize();
+        uz.Normalize();
+
+        // Project the particle on the box axis : ux, uy and uz
+        float px = Vector3.Dot(D, ux);
+        float py = Vector3.Dot(D, uy);
+        float pz = Vector3.Dot(D, uz);
+
+        return Mathf.Abs(px) <= p_obb.size.x && Mathf.Abs(py) <= p_obb.size.y && Mathf.Abs(pz) <= p_obb.size.z;
     }
 
     private void UpdateRotation()
@@ -83,38 +76,13 @@ public class Test : MonoBehaviour
                                      new Vector4(0f, 0f, 1f, 0f),
                                      new Vector4(0f, 0f, 0f, 1f));
 
-        _rotMatrix = Rx * Ry * Rz;
+        _bounds.rotation = Rx * Ry * Rz;
     }
 
     private void OnDrawGizmos()
     {
-        // Cube points
-        Vector3 p0 = _rotMatrix * new Vector3(-1f, -1f, -1f);
-        Vector3 p1 = _rotMatrix * new Vector3(-1f,  1f, -1f);
-        Vector3 p2 = _rotMatrix * new Vector3( 1f, -1f, -1f);
-        Vector3 p3 = _rotMatrix * new Vector3( 1f,  1f, -1f);
-        Vector3 p5 = _rotMatrix * new Vector3(-1f, -1f,  1f);
-        Vector3 p6 = _rotMatrix * new Vector3(-1f,  1f,  1f);
-        Vector3 p7 = _rotMatrix * new Vector3( 1f, -1f,  1f);
-        Vector3 p8 = _rotMatrix * new Vector3( 1f,  1f,  1f);
-
-        // Min
-        Gizmos.color = Color.red;
-        Gizmos.DrawSphere(p0 * _boxSize, _pointSize);
-        _min = new Vector3(-1f, -1f, -1f) * _boxSize;
-        Gizmos.color = Color.white;
-        
-        Gizmos.DrawSphere(p1 * _boxSize, _pointSize);
-        Gizmos.DrawSphere(p2 * _boxSize, _pointSize);
-        Gizmos.DrawSphere(p3 * _boxSize, _pointSize);
-        Gizmos.DrawSphere(p5 * _boxSize, _pointSize);
-        Gizmos.DrawSphere(p6 * _boxSize, _pointSize);
-        Gizmos.DrawSphere(p7 * _boxSize, _pointSize);
-
-        // Max
-        Gizmos.color = Color.red;
-        Gizmos.DrawSphere(p8 * _boxSize, _pointSize);
-        _max = new Vector3(1f, 1f, 1f) * _boxSize;
-        Gizmos.color = Color.white;
+        Gizmos.matrix = _bounds.rotation;
+        Gizmos.DrawCube(Vector3.zero, transform.localScale);
+        Gizmos.matrix = Matrix4x4.identity;
     }
 }
