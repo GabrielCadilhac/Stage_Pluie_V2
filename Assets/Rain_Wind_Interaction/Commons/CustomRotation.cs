4ff4435da8635a53429128b301526272e126487f
GabrielCadilhac <59889782+GabrielCadilhac@users.noreply.github.com>
2025-01-08 09:08:03 +0100

    ~OBB collision correction and code optimisations

--- Assets/Rain_Wind_Interaction/Commons/CustomRotation.cs
+++ Assets/Rain_Wind_Interaction/Commons/CustomRotation.cs
@@ -6,13 +6,35 @@ public class CustomRotation : MonoBehaviour
 {
     [SerializeField] private Vector3 _rotation;
 
+    private Matrix4x4 _rotMat;
+
     private void Update()
     {
-        transform.rotation = Quaternion.Euler(_rotation);
+        Vector3 angles = _rotation * Mathf.Deg2Rad; // In radians
+        Vector3 c = new Vector3(Mathf.Cos(angles.x), Mathf.Cos(angles.y), Mathf.Cos(angles.z));
+        Vector3 s = new Vector3(Mathf.Sin(angles.x), Mathf.Sin(angles.y), Mathf.Sin(angles.z));
+
+        Matrix4x4 Rx = new Matrix4x4(new Vector4(1f, 0f, 0f, 0f),
+                                     new Vector4(0f, c.x, s.x, 0f),
+                                     new Vector4(0f, -s.x, c.x, 0f),
+                                     new Vector4(0f, 0f, 0f, 1f));
+
+        Matrix4x4 Ry = new Matrix4x4(new Vector4(c.y, 0f, s.y, 0f),
+                                     new Vector4(0f, 1f, 0f, 0f),
+                                     new Vector4(-s.y, 0f, c.y, 0f),
+                                     new Vector4(0f, 0f, 0f, 1f));
+
+        Matrix4x4 Rz = new Matrix4x4(new Vector4(c.z, -s.z, 0f, 0f),
+                                     new Vector4(s.z, c.z, 0f, 0f),
+                                     new Vector4(0f, 0f, 1f, 0f),
+                                     new Vector4(0f, 0f, 0f, 1f));
+        _rotMat = Rx * Ry * Rz;
+
+        transform.rotation = _rotMat.rotation;
     }
 
-    public Vector3 GetRotation()
+    public Matrix4x4 GetRotation()
     {
-        return _rotation;
+        return _rotMat;
     }
 }
--- Assets/Rain_Wind_Interaction/Rain/RainGenerator.cs
+++ Assets/Rain_Wind_Interaction/Rain/RainGenerator.cs
@@ -97,30 +97,10 @@ public class RainGenerator
         for (int i = 0; i < oBBs.Length; i++)
         {
             GameObject go = p_obbsGameObject[i];
-            Vector3 angles = go.GetComponent<CustomRotation>().GetRotation() * Mathf.Deg2Rad; // In radians
-            angles = new Vector3(angles.x, -angles.y, -angles.z); // Invert y and z (Unity coordinates system)
-
-            Vector3 c = new Vector3(Mathf.Cos(angles.x), Mathf.Cos(angles.y), Mathf.Cos(angles.z));
-            Vector3 s = new Vector3(Mathf.Sin(angles.x), Mathf.Sin(angles.y), Mathf.Sin(angles.z));
-
-            Matrix4x4 Rx = new Matrix4x4(new Vector4(1f, 0f, 0f, 0f),
-                                         new Vector4(0f, c.x, s.x, 0f),
-                                         new Vector4(0f, -s.x, c.x, 0f),
-                                         new Vector4(0f, 0f, 0f, 1f));
-
-            Matrix4x4 Ry = new Matrix4x4(new Vector4(c.y, 0f, s.y, 0f),
-                                         new Vector4(0f, 1f, 0f, 0f),
-                                         new Vector4(-s.y, 0f, c.y, 0f),
-                                         new Vector4(0f, 0f, 0f, 1f));
-
-            Matrix4x4 Rz = new Matrix4x4(new Vector4(c.z, -s.z, 0f, 0f),
-                                         new Vector4(s.z, c.z, 0f, 0f),
-                                         new Vector4(0f, 0f, 1f, 0f),
-                                         new Vector4(0f, 0f, 0f, 1f));
 
             // Define the OBB
             OBB oBB = new OBB();
-            oBB.rotation = Rx * Ry * Rz;
+            oBB.rotation = go.GetComponent<CustomRotation>().GetRotation();
             oBB.center   = go.transform.position;
             oBB.size     = go.transform.localScale / 2f;
 
